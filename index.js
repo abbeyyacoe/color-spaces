699cb6f7e1f670153de88046177a8288268f233b
abbey <abbeyyacoe@gmail.com>
2021-06-23 14:59:50 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 // Code By Webdevtrick ( https://webdevtrick.com )
 var body = document.body,
-    r = document.querySelector('#r'),
-    g = document.querySelector('#g'),
-    b = document.querySelector('#b'),
-    outputR = document.querySelector('#outputR'),
-    outputG = document.querySelector('#outputG'),
-    b_out = document.querySelector('#b_out'),
     rhexVal_out = document.querySelector('#final_hexVal_r');
     ghexVal_out = document.querySelector('#final_hexVal_g');
     bhexVal_out = document.querySelector('#final_hexVal_b');
@@ -13,30 +7,20 @@ var body = document.body,
     body.style.color = "#ffffff";
 
 function setColor(){
-  var r_hexVal = parseInt(r.value, 10).toString(16),
-      g_hexVal = parseInt(g.value, 10).toString(16),
-      b_hexVal = parseInt(b.value, 10).toString(16),
+  var r_hexVal = pad(parseInt(r.value, 10).toString(16)),
+      g_hexVal = pad(parseInt(g.value, 10).toString(16)),
+      b_hexVal = pad(parseInt(b.value, 10).toString(16))
       // reverse values
-      reverse_r = parseInt(255 - r.value, 10).toString(16),
-      reverse_g = parseInt(255 - g.value, 10).toString(16),
-      reverse_b = parseInt(255 - b.value, 10).toString(16),
+      reverse_r = pad(parseInt(255 - r.value, 10).toString(16)),
+      reverse_g = pad(parseInt(255 - g.value, 10).toString(16)),
+      reverse_b = pad(parseInt(255 - b.value, 10).toString(16)),
 
-      final_hexVal_r =  pad(r_hexVal);
-      final_hexVal_g =  pad(g_hexVal);
-      final_hexVal_b = pad(b_hexVal);
+      body.style.backgroundColor = "#" + r_hexVal + g_hexVal + b_hexVal;
+      reverse_value.style.color = "#" + reverse_r + reverse_g + reverse_b;
 
-      reverse_hexVal_r =  pad(reverse_r);
-      reverse_hexVal_g =  pad(reverse_g);
-      reverse_hexVal_b = pad(reverse_b);
-
-      body.style.backgroundColor = "#" + final_hexVal_r + final_hexVal_g + final_hexVal_b;
-      reverse_value.style.color = "#" + reverse_hexVal_r + reverse_hexVal_g + reverse_hexVal_b;
-      rhexVal_out.value = final_hexVal_r;
-      ghexVal_out.value = final_hexVal_g;
-      bhexVal_out.value = final_hexVal_b;
-      rhexVal_out.style.color = "#" + reverse_hexVal_r + reverse_hexVal_g + reverse_hexVal_b;
-      ghexVal_out.style.color = "#" + reverse_hexVal_r + reverse_hexVal_g + reverse_hexVal_b;
-      bhexVal_out.style.color = "#" + reverse_hexVal_r + reverse_hexVal_g + reverse_hexVal_b;
+      rhexVal_out.value = r_hexVal;
+      ghexVal_out.value = g_hexVal;
+      bhexVal_out.value = b_hexVal;
 }
 
 function pad(n){
